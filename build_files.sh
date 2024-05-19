8e1620bd482500374689d97596073b37c82e2b87
AaronTech112 <abdullahiaaron112@gmail.com>
2024-05-19 17:51:56 +0100

    vercel deployment fix1

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+#!/bin/bash
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
